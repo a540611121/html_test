2b9b985394e4a312f73f50a9c67a5e826c8ed118
raymondln <540611121@qq.com>
2023-04-07 01:09:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-// 显示输入框
-function showInput() {
-    var input = document.getElementById("input");
-    input.style.display = "block";
-    input.focus();
+// 定义雪花数量
+var NUMBER_OF_FLAKES = 50;
+
+// 获取容器元素
+var container = document.querySelector('.snowflakes');
+
+// 创建雪花元素
+for (var i = 0; i < NUMBER_OF_FLAKES; i++) {
+    var flake = document.createElement('div');
+    flake.classList.add('snowflake');
+    container.appendChild(flake);
+
+    // 随机设置雪花的位置和速度
+    flake.style.left = Math.random() * 100 + '%';
+    flake.style.animationDuration = Math.random() * 3 + 2 + 's';
 }
 
-// 监听回车事件，并获取用户输入
-var input = document.getElementById("input");
-input.addEventListener("keypress", function(event) {
-    if (event.keyCode === 13) {
-        var message = input.value;
-        alert("您的回复是：" + message);
-    }
-});
